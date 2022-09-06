7a17bf0098261f3e4baa96c328d072361467f2b2
Sellech <iaroslavchepiuk@gmail.com>
2022-09-06 17:16:07 +0300

    input of N

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        
+        int N = 0;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Choose an input method:\n\t0 - console\n\t1 - command line");
+
+        byte readMode = in.nextByte();
+        if(readMode == 0){
+            System.out.println("Enter the value of N: ");
+            N = in.nextInt();
+        }
+        else if(readMode == 1){
+            N = Integer.parseInt(args[0]);
+        }
+        else{
+            System.out.println("You entered an incorrect value.\nEnd of the program.");
+        }
     }
 }
